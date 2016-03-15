39b31779fcc93fcaa6e3a5331aa18f018e648dfa
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2016-03-15 07:47:02 +0100

    reduce sleeps in test class

--- src/test/java/pl/mdettlaff/tracingagent/SampleController.java
+++ src/test/java/pl/mdettlaff/tracingagent/SampleController.java
@@ -7,22 +7,22 @@ public class SampleController {
 
 	public static void main(String[] args) throws Exception {
 		SampleController controller = new SampleController();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 5; i++) {
 			controller.init();
 			controller.other();
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 	}
 
 	public void init() throws InterruptedException {
 		System.out.println("doing init stuff");
-		Thread.sleep(1000);
+		Thread.sleep(100);
 		System.out.println("done init stuff");
 	}
 
 	public void other() throws InterruptedException {
 		System.out.println("doing other stuff");
-		Thread.sleep(1000);
+		Thread.sleep(100);
 		System.out.println("done other stuff");
 	}
 }
